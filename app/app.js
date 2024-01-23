08f73851ec17e00bc12969f26195a7dd93ee6f13
maggieslei <leimaggiese@gmail.com>
2024-01-22 21:32:38 -0500

    chore: server listener router design pattern

---
+++ app/app.js
@@ -0,0 +1,33 @@
+const cors = require('cors');
+
+const userRouter = require('../router/userRouter');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// health point or actuator
+// http://localhost:3001
+app.get('/', (req, res, next) => {
+  res.status(200).json({ message: 'Service is up' });
+});
+
+app.use('/users', userRouter);
+
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    error: {
+      status: error.status,
+      message: error.message,
+    },
+  });
+});
+module.exports = app;
